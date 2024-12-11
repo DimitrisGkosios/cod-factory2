c74cfb5d89004efa47ecd7539331d93eb8c334de
Gkosios Dimitrios <dgkosios@gmail.com>
2024-12-11 13:14:15 +0200

    initial commit

---
+++ gr/aueb/cf/ch1/Test2.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch1;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 1;
+        int result = num1 + num2;
+
+        System.out.println("The result is :"+ result);
+    }
+}
---
+++ gr/aueb/cf/ch1/Test3.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch1;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+        System.out.println("error 23423");
+        System.out.println("error 1234");
+    }
+}
